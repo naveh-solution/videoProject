e0b3e0feb2400aadcab5a44b1ffa49f1ecef8945
guy_nveh <guyznave@gmail.com>
2021-07-25 16:37:35 +0300

    adding boot strap, creating the actor array start

---
+++ src/components/cp-card.jsx
@@ -0,0 +1,3 @@
+function CardComp({actor}){
+    
+}
---
+++ src/model/actors.js
@@ -0,0 +1,43 @@
+class Actor{
+    constructor (fname, lname, bDay, picLink, imdbLink){
+        this.fname=fname;
+        this.lname=lname;
+        this.bDay=bDay;
+        this.picLink=picLink;
+        this.imdbLink=imdbLink;
+    }
+
+    
+}
+
+
+const actors = [{
+    fname: "Matt",
+    lname: "LeBlanc",
+    bDay: "25-7-1967",
+    picLink: "../imgs/mattLeBlanc.jpg",
+    imdbLink: "https://www.imdb.com/name/nm0001455/",
+},
+{
+    fname: "David Bryan",
+    lname: "Woodside",
+    bDay: "25-7-1969",
+    picLink: "../imgs/DBWoodside.jpg",
+    imdbLink: "https://www.imdb.com/name/nm0940851/", 
+},
+{
+    fname: "Nicolas",
+    lname: "Cage",
+    bDay: "7-1-1964",
+    picLink: "../imgs/nicolasCage.jpg",
+    imdbLink: "https://www.imdb.com/name/nm0000115/", 
+},
+{
+    fname: "Julia",
+    lname: "Roberts",
+    bDay: "28-10-1967",
+    picLink: "../imgs/mattLeBlanc.jpg",
+    imdbLink: "https://www.imdb.com/name/nm0001455/", 
+},
+]
+
